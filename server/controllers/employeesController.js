2be62164de57babbe5f688e81549d25a27c398ea
ChimaBright <blakechima@gmail.com>
2026-05-18 10:44:10 +0100

    Backend Server

---
+++ server/controllers/employeesController.js
@@ -0,0 +1,24 @@
+
+
+// Get Employees
+export const getEmployees = async (req, res) => {
+    
+}
+
+// Create Employees
+// POST /api/employees
+export const createEmployees = async (req, res) => {
+    
+}
+
+// Update Employees
+// PUT /api/employees/:id
+export const updateEmployees = async (req, res) => {
+    
+}
+
+// Delete Employees
+// DELETE /api/employees/:id
+export const deleteEmployees = async (req, res) => {
+    
+}
